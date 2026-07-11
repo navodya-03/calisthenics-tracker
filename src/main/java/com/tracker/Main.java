4d9960a7fecdff333c2d1e548fc286a0729d9651
Navodya Dissanayake <nawodhyasrilanka2015@gmail.com>
2026-07-11 16:13:21 +0530

    feat: build interactive CLI menu with exception handling

--- src/main/java/com/tracker/Main.java
+++ src/main/java/com/tracker/Main.java
@@ -1,5 +1,12 @@
 package com.tracker; // <-- Tells Java this is in the tracker folder
 
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.tracker.exception.InvalidExerciseCalorieBurned;
+import com.tracker.exception.InvalidExerciseSets;
+import com.tracker.exception.InvalidWorkoutDataException;
 import com.tracker.model.Exercise; // <-- Tells Java where to find the Exercise model
 import com.tracker.model.Workout;
 import com.tracker.repository.WorkoutRepository;
@@ -8,81 +15,203 @@ import com.tracker.service.WorkoutAnalyzer;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Calisthenics and HIIT Tracker");
-
-		Workout shoulderWorkout = new Workout("s1", "6/30/2026", "Upper-Body");
-		Workout chestWorkout = new Workout("c1", "7/1/2026", "Upper-Body");
-		Workout backWorkout = new Workout("b1", "7/1/2026", "Upper-Body");
-		Workout tricepWorkout = new Workout("t1", "7/1/2026", "Upper-Body");
-
-
-
-		WorkoutRepository workouts = new WorkoutRepository();
-		WorkoutAnalyzer analyzer = new WorkoutAnalyzer(workouts);
-
-		
-
-		workouts.save("t1",tricepWorkout);
-		workouts.save("c1",chestWorkout);
-		workouts.save("b1",backWorkout);
-		workouts.save("s1",shoulderWorkout);
-
-		
-
-        Exercise newExercise1 = new Exercise("Pike Push-ups", "Strength", 3, 12, 0);
-		Exercise newExercise2 = new Exercise("Dumbell Overhead Press", "Strength", 3, 12, 0);
-
-		Exercise newExercise3 = new Exercise("Weighted Pull-Ups", "Strength", 3, 12, 0);
-		Exercise newExercise4 = new Exercise("one sided dumbell rows", "Strength", 3, 12, 0);
-
-		Exercise newExercise5 = new Exercise("Weighted Push-ups", "Strength", 3, 12, 30);
-		Exercise newExercise6 = new Exercise("Bench press", "Strength", 3, 12, 25);
-
-		Exercise newExercise7 = new Exercise("Diamond Push-ups", "Strength", 3, 12, 0);
-		Exercise newExercise8 = new Exercise("Cable push downs", "Strength", 3, 12, 0);
-
-		newExercise1.setReps(-5);
-
-		shoulderWorkout.addExercises(newExercise2);
-		shoulderWorkout.addExercises(newExercise1);
-
-		chestWorkout.addExercises(newExercise5);
-		chestWorkout.addExercises(newExercise6);
-
-		tricepWorkout.addExercises(newExercise8);
-		tricepWorkout.addExercises(newExercise7);
-
-		backWorkout.addExercises(newExercise3);
-		backWorkout.addExercises(newExercise4);
-
+		Scanner scanner = new Scanner(System.in);
 		
 		
-		showDetails(workouts.findById("t1"));
-		showDetails(workouts.findById("c1"));
-		showDetails(workouts.findById("s1"));
-		showDetails(workouts.findById("b1"));
 
-		;
+		int option = 0;
+		while (true) {
+			System.out.println("1. Add Workout");
+			System.out.println("2. View All");
+			System.out.println("3. Analyze");
+			System.out.print("4. Exit");
 
-
-		System.out.println("### SIZE OF THE WORKOUT REPOSITORY: "+ workouts.findAll().size());
-
-		System.out.println("\n\n\n\n\n\n\n");
-
-		System.out.println("-------------------------------------WORKOUTS GET BY FOCUS AREA ------------------------------------");
-
-		analyzer.getWorkoutsByFocus("Upper-Body").forEach(System.out::println);
-		System.out.println("Total calories burned by chest workout "+ analyzer.calculateTotalCaloriesBurned("c1"));
-		
-    }
-
-	public static void showDetails(Workout workout){
-		System.out.println("---- Details of Workout ID "+ workout.getId()+ " ----");
+			option = scanner.nextInt();
+			scanner.nextLine();
+			
 
 			
-		System.out.println(workout.getId());
-		System.out.println(workout.getDate());
-		System.out.println(workout.getFocusArea());
-		workout.listAllExercises();
+			
+			
+			
+			switch(option){
+				case 1:
+					Scanner workoutScanner = new Scanner(System.in);
 
+					while (true) {
+
+						System.out.println("enter the workout id: ");
+						String id = workoutScanner.nextLine();
+
+						System.out.println("enter the date: ");
+						String date = workoutScanner.nextLine();
+
+						System.out.println("enter the focus Area: ");
+						String focusArea = workoutScanner.nextLine();
+
+						Workout newWorkout = new Workout(id, date, focusArea);
+
+						System.out.println("do you want to create another workout:[y/n] ");
+						String input = workoutScanner.nextLine();
+
+						if(input.equalsIgnoreCase("y")){
+							continue;
+						}else{
+							System.out.println(newWorkout.toString());
+							break;
+						}
+						
+					}
+
+					
+
+					
+
+
+					
+					break;
+				default:
+					System.out.println("nothing enter a  number");		
+						
+
+
+
+						
+					
+
+						
+						
+		
+			}
 		}
+	}
+
+	public static void createExercise(){
+		Scanner scanner = new Scanner(System.in);
+		List <String> types = List.of("aerobic","strenght","flexibility","balance","metabolic");
+
+		String name;
+					while (true) {
+						System.out.println("You have to create a Exercises in order to create a Workout!");
+						System.out.println("Enter Exercise name: ");
+						name = scanner.nextLine().toLowerCase();
+						if(!name.matches("[a-z]+")){
+							System.out.println("invalid input use only letters when creating the name!");
+							continue;
+						}else{
+							break;
+						}
+					}	
+
+					String type;
+					while (true) {
+
+						System.out.println("Enter the Exercise type(it should be one out of (aerobic,strenght,flexibility,balance,metabolic) ): ");
+						type = scanner.nextLine().trim().toLowerCase();
+						if (!types.contains(type)) {
+							System.out.println("invalid input try one from the mentioned list");
+							continue;
+						}else{
+							break;
+						}
+						
+					}
+					int input;
+					while (true) {
+						try {
+							System.out.println("enter number of reps");
+							input = scanner.nextInt();
+							scanner.nextLine();
+							
+						
+						}catch (Exception e) {
+							System.out.println(e.toString());
+							scanner.nextLine();
+							continue;	
+						}
+
+						try{
+							if (input<0) {
+								throw new InvalidWorkoutDataException("wrong");
+								
+						}else{
+							break;
+
+						}
+						
+						}catch(InvalidWorkoutDataException e){
+							System.out.println(e.toString());
+						}
+						
+						
+					}
+				
+					int sets;
+					while (true) {
+						System.out.println("Enter the number of sets");
+						
+
+						try{
+							sets = scanner.nextInt();
+
+						}catch(InputMismatchException e){
+							System.out.println("wrong type of input insert a integer value");
+							scanner.nextLine();
+							continue;
+						}
+
+						try{
+							if (sets<0) {
+								throw new InvalidExerciseSets("this can not be negative enter positive value");
+								
+							}else{
+								break;
+							}
+						}catch(InvalidExerciseSets e){
+							System.out.println(e.toString());
+							continue;
+						}
+					}
+						double calories;
+						while (true) {
+							System.out.println("enter the number of calories burned: ");
+							
+
+							try{
+								calories = scanner.nextDouble();
+
+							}catch(InputMismatchException e){
+								System.out.println(e.toString());
+								scanner.nextLine();
+								continue;
+							}
+
+							try{
+								if (calories<0) {
+									
+									throw new InvalidExerciseCalorieBurned("invalid type try double value");
+									
+								}else{
+									break;
+								}
+
+
+								}catch (InvalidExerciseCalorieBurned e){
+
+									System.out.println(e.toString());
+									continue;
+							}
+							
+						}
+						
+
+						Exercise newExercise = new Exercise(name,type,sets,input,calories);
+	}	
 }
+
+
+			
+		
+
+	
+
