8c90fe5b48dcac7b6dc2600f8213ecf13a53e24c
Navodya Dissanayake <nawodhyasrilanka2015@gmail.com>
2026-07-11 16:10:21 +0530

    feat: Invalid calories burned and sets introduced

---
+++ src/main/java/com/tracker/exception/InvalidExerciseSets.java
@@ -0,0 +1,12 @@
+package com.tracker.exception;
+
+public class InvalidExerciseSets extends RuntimeException{
+
+    public InvalidExerciseSets(String message){
+        super(message);
+    }
+
+
+
+    
+}
